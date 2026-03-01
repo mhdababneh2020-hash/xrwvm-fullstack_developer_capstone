3d81eac91bb9e6220f0464d7e706922ab3a5a944
mhdababneh2020-hash <mhdababneh2020@outlook.com>
2026-03-01 13:28:30 -0500

    Modify the About.html file

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,8 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
+urlpatterns = [   
+    path('about/',TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
