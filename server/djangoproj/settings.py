81bb9c6cd29ee9c3e751955a15720334b359c6c4
Mohammad Ababneh <mhdababneh2020@outlook.com>
2026-06-16 15:47:50 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,12 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    'mhdababneh20-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-]
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
-    'https://mhdababneh20-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://xrwvm-fullstack-developer-capstone-5gr5.onrender.com'
 ]
 
 REST_FRAMEWORK = {
